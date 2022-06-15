a5123702af946b318fb1e8737ce324101819e960
DESKTOP-VC8EV58\Kaiwei <skybirdlandcowseafish@gmail.coom>
2022-06-15 09:53:13 +0800

    Tidy up the files and folder

--- credentials.py
+++
@@ -1,5 +0,0 @@
-#Shopify
-APIKEY = "1da062b3aea0f3a1a3eed35d52510c20"
-PASSWORD = "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" #CONFIDENTIAL
-HOSTNAME = "TheKettleGourmet.myshopify.com"
-VERSION = "2022-04"
