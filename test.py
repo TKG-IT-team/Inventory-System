783a328f526312e4de1ff6ad0308e4649dddd528
Zhen Yu <zychin000@gmail.com>
2022-07-13 14:42:09 +0800

    Update product setting file (by ian)

--- test.py
+++ test.py
@@ -1,36 +1,12 @@
-# import logging
-# import os
+import pandas as pd
 
-# logger = logging.getLogger(__name__)
-# logger.setLevel(logging.DEBUG)
-# formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
-# file_handler = logging.FileHandler(f"{os.path.dirname(__file__)}/error.log", mode="w")
-# file_handler.setFormatter(formatter)
-# logger.addHandler(file_handler)
+combined_data = pd.read_excel("Combined Data.xlsx")
+index_list = []
+for i, value in combined_data["Name"].iteritems():
+    if value == None or value == "":
+        index_list.append(i)    
 
-# logger.warning("Hello")
-
-from xmlrpc.client import Unmarshaller
+print(index_list)
 
 
-unmatched_names = ""
-unmatched_names += "hello"+ " Not Found in Customer Data"
-
-print(unmatched_names)
-
-
-# input()
-
-# class logger():
-#     def __init__(self):
-#         logger = logging.getLogger(__name__)
-#         logger.setLevel(logging.DEBUG)
-#         formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
-#         file_handler = logging.FileHandler("error.log", mode="w")
-#         file_handler.setFormatter(formatter)
-#         logger.addHandler(file_handler)
-#         self.logger = logger
-#     def get_logger(self):
-#         return self.logger
-
-# loggerr = logger()
+# print("Hell0".__contains__("S", "e"))
