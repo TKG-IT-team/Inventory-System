2e9a9b9756ee1327b5d17b65bdb247255304a424
DESKTOP-VC8EV58\Kaiwei <skybirdlandcowseafish@gmail.coom>
2022-07-15 15:12:45 +0800

    Drop three useless customer column

--- Lazada/LazadaOrderAPI.py
+++ Lazada/LazadaOrderAPI.py
@@ -168,10 +168,10 @@ def get_orders(last_created_after = '2021-01-01T00:00:00+08:00'):
 #Cleans data in the dataframe
 def clean_df(df):
   
-    df = df[['order_id', 'created_at',  'status', 'Notes', 'paid_price', 'currency', 'name' , 'phone', 'address', 'first_name']] # keep needed columns
+    df = df[['order_id', 'created_at',  'status', 'Notes', 'name' , 'phone', 'address', 'first_name']] # keep needed columns
 
     df = df.rename(columns={"order_id":"Order No.", "created_at":"Created At","status":"Fulfillment Status", "remark":"Notes",  #Renames columns
-    "phone":"HP", "address":"Address", "first_name":"Name", "paid_price":"Amount Spent", "currency":"Currency","name":"Product" })
+    "phone":"HP", "address":"Address", "first_name":"Name","name":"Product" })
 
     # df["Product"] = "{'" + df["Product"].values + "':1}"
     df["Platform"] = "Lazada"
